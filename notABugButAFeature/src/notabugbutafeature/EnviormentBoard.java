7bb16e46354f4e60ed0d60c63b1338acb48f2a26
yuchenpan <yuchenpan88@gmail.com>
2017-11-11 20:06:00 +0000

    Added second rectangle onto board. Checks if the first rectangle is overlapping with the one on screen and stop them from moving if they are. Can be used to enter a quest state.

--- notABugButAFeature/src/notabugbutafeature/EnviormentBoard.java
+++ notABugButAFeature/src/notabugbutafeature/EnviormentBoard.java
@@ -3,23 +3,29 @@ package notabugbutafeature;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Rectangle;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
 
-public class Board extends JPanel implements ActionListener {
+public class EnviormentBoard extends JPanel implements ActionListener {
 
     private Timer timer;
     private Craft craft;
     private final int DELAY = 10;
+    
+    private final JPanel enviormentTutor = new JPanel();
+    private Image environmentTutorImage;
+    private final Rectangle environmentTutorCollisionRectangle = new Rectangle(1000, 400, 70, 70);
 
-    public Board() {
-
+    public EnviormentBoard() {
         initBoard();
     }
     
@@ -29,7 +35,10 @@ public class Board extends JPanel implements ActionListener {
         setFocusable(true);
         setBackground(Color.BLACK);
 
-        craft = new Craft();
+        craft = new Craft(this);
+        ImageIcon environmentTutorImageIcon = new ImageIcon("red_square.png");
+        environmentTutorImage = environmentTutorImageIcon.getImage();
+        
 
         timer = new Timer(DELAY, this);
         timer.start();        
@@ -45,15 +54,19 @@ public class Board extends JPanel implements ActionListener {
         Toolkit.getDefaultToolkit().sync();
     }
 
-    private void doDrawing(Graphics g) {
-        
+    private void doDrawing(Graphics g) {       
         Graphics2D g2d = (Graphics2D) g;
-        g2d.drawImage(craft.getImage(), craft.getX(), craft.getY(), this);        
+        g2d.drawImage(craft.getImage(), craft.getX(), craft.getY(), this);
+        g2d.drawImage(environmentTutorImage, 1000, 400, this);          
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        repaint();  
+        repaint();
+    }
+    
+    public boolean overlaps (Rectangle r) {
+        return(environmentTutorCollisionRectangle.intersects(r));
     }
 
     private class TAdapter extends KeyAdapter {
