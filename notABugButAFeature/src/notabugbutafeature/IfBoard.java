cf5557b6a477b0cd62d2334861c9e85f2e050197
Delia Savin <delia_savin@yahoo.com>
2017-11-11 20:43:57 +0000

    Started the IfBoard

---
+++ notABugButAFeature/src/notabugbutafeature/IfBoard.java
@@ -0,0 +1,76 @@
+package notabugbutafeature;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+
+public class IfBoard extends JPanel implements ActionListener {
+
+    private Timer timer;
+    private Craft craft;
+    private BlueSquare character;
+    private final int DELAY = 10;
+
+    public IfBoard() {
+
+        initBoard();
+    }
+    
+    private void initBoard() {
+        
+        addKeyListener(new TAdapter());
+        setFocusable(true);
+        setBackground(Color.GRAY);
+
+        craft = new Craft();
+        character = new BlueSquare();
+
+        timer = new Timer(DELAY, this);
+        timer.start();        
+    }
+
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        doDrawing(g);
+
+        Toolkit.getDefaultToolkit().sync();
+    }
+
+    private void doDrawing(Graphics g) {
+        
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.drawImage(craft.getImage(), craft.getX(), craft.getY(), this);  
+        g2d.drawImage(character.getImage(), character.getX(), character.getY(), this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        repaint();  
+    }
+
+    private class TAdapter extends KeyAdapter {
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            craft.keyReleased(e);
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            craft.keyPressed(e);
+        }
+        
+        
+    }
+}
