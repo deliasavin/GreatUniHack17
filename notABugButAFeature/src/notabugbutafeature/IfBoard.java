0b6d5c0540ba84f01ac1f6c6ff44bf5b84d3247a
deliasavin <delia_savin@yahoo.com>
2017-11-12 01:39:22 +0000

    Started working on the chat bubbles

--- notABugButAFeature/src/notabugbutafeature/IfBoard.java
+++ notABugButAFeature/src/notabugbutafeature/IfBoard.java
@@ -1,15 +1,21 @@
 package notabugbutafeature;
 
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import javax.swing.JPanel;
 import javax.swing.Timer;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Polygon;
+import java.awt.RenderingHints;
+import java.awt.geom.Area;
+import java.awt.geom.RoundRectangle2D;
+import javax.swing.JPanel;
 
 
 public class IfBoard extends MasterBoard implements ActionListener {
@@ -70,7 +76,32 @@ public class IfBoard extends MasterBoard implements ActionListener {
         public void keyPressed(KeyEvent e) {
             craft.keyPressed(e);
         }
-        
-        
     }
+    
+public class LeftArrowBubble extends JPanel {
+   private static final long serialVersionUID = -5389178141802153305L;
+   private int radius = 10;
+   private int arrowSize = 12;
+   private int strokeThickness = 3;
+   private int padding = strokeThickness / 2;
+   @Override
+   protected void paintComponent(final Graphics g) {
+      final Graphics2D g2d = (Graphics2D) g;
+      g2d.setColor(new Color(0.5f, 0.8f, 1f));
+      int x = padding + strokeThickness + arrowSize;
+      int width = getWidth() - arrowSize - (strokeThickness * 2);
+      int bottomLineY = getHeight() - strokeThickness;
+      g2d.fillRect(x, padding, width, bottomLineY);
+      g2d.setRenderingHints(new RenderingHints(RenderingHints.KEY_ANTIALIASING,   RenderingHints.VALUE_ANTIALIAS_ON));
+      g2d.setStroke(new BasicStroke(strokeThickness));
+      RoundRectangle2D.Double rect = new RoundRectangle2D.Double(x, padding, width, bottomLineY, radius, radius);
+      Polygon arrow = new Polygon();
+      arrow.addPoint(20, 8);
+      arrow.addPoint(0, 10);
+      arrow.addPoint(20, 12);
+      Area area = new Area(rect);
+      area.add(new Area(arrow));
+      g2d.draw(area);
+   }
+}
 }
